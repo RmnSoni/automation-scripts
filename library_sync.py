de03b24802e26b0a773a0313d14b01e1ec90995b
gasher <69788442+RmnSoni@users.noreply.github.com>
2023-10-21 00:23:00 +0530

    chamged formatting

--- library_sync.py
+++ library_sync.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-print ("Hello World")
 
 path = '/home/gasher/Downloads/'
 
@@ -11,10 +10,13 @@ accepted_formats = [ '.epub', '.mobi', '.awz' ]
 for item in listed:
     if any(item.endswith(ext) for ext in accepted_formats):
 
-        # subprocess.Popen( 'calibredb add "'+path+item+'"', shell=True)
         try:
-            result = subprocess.check_output(["calibredb", "add", "--automerge=new_record", path + item], universal_newlines=True, stderr=subprocess.STDOUT)
+            result = subprocess.check_output(
+                ["calibredb", "add", "--automerge=new_record", path + item],
+                universal_newlines=True,
+                stderr=subprocess.STDOUT)
             print(f"Added '{item}' to Calibre library. Result:\n{result}")
             os.remove(path + item)
+        
         except subprocess.CalledProcessError as e:
             print(f"Error adding '{item}' to Calibre library. Error message:\n{e.output}")
